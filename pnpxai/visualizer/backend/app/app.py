1aad209941c53858db10532df9966d934333a23c
Enver Menadjiev <enver1323@gmail.com>
2023-12-11 13:48:33 +0900

    chore: move app serving to a separate file

--- pnpxai/visualizer/backend/app/app.py
+++ pnpxai/visualizer/backend/app/app.py
@@ -1,12 +1,19 @@
 from pnpxai.visualizer.backend.app.core.init_modules import init, InitConfig
-from flask import Flask, send_from_directory
+from flask import Flask
 import os
+
 
 def create_app(projects: dict):
     # Correct the path to the build directory
-    frontend_build_path = os.path.join(os.path.dirname(__file__), '../../../frontend/build')
+    frontend_build_path = os.path.join(
+        os.path.dirname(__file__), '../../../frontend/build'
+    )
 
-    application = Flask(__name__, static_folder=frontend_build_path, static_url_path='/')
+    application = Flask(
+        __name__,
+        static_folder=frontend_build_path,
+        static_url_path='/'
+    )
     # application.config.from_object(Config)
     init(application, InitConfig(projects=projects))
 
@@ -14,12 +21,7 @@ def create_app(projects: dict):
     application.register_blueprint(api_bp)
 
     # Serve static files and index.html for routes not in api_bp
-    @application.route('/', defaults={'path': ''})
-    @application.route('/<path:path>')
-    def serve(path):
-        if path and os.path.exists(application.static_folder + '/' + path):
-            return send_from_directory(application.static_folder, path)
-        else:
-            return send_from_directory(application.static_folder, 'index.html')
+    from pnpxai.visualizer.backend.app.routing.web import web_bp
+    application.register_blueprint(web_bp)
 
     return application
