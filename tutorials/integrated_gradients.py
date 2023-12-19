aae16e35379e7936c3bfe3909b6a625b220a2bcc
gkim <gkim9213@gmail.com>
2023-12-19 09:13:49 +0900

    feat: modify ig tutorial

--- tutorials/integrated_gradients.py
+++ tutorials/integrated_gradients.py
@@ -1,92 +1,31 @@
-import os
+from torch.utils.data import DataLoader
 
-import matplotlib.pyplot as plt
-import numpy as np
+from pnpxai.explainers import IntegratedGradients, ExplainerWArgs
+from pnpxai.evaluator import XaiEvaluator
+from pnpxai.evaluator.mu_fidelity import MuFidelity
+from pnpxai.evaluator.sensitivity import Sensitivity
+from pnpxai.evaluator.complexity import Complexity
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from helpers import get_imagenet_dataset, get_torchvision_model
 
-import torchvision
-import torchvision.transforms as transforms
-import torchvision.transforms.functional as TF
-from torchvision import models
+model, transform = get_torchvision_model("resnet18")
+dataset = get_imagenet_dataset(transform=transform, subset_size=100)
+loader = DataLoader(dataset, batch_size=8)
+inputs, labels = next(iter(loader))
+targets = labels
 
-from pnpxai import Project
-from pnpxai.explainers import IntegratedGradients
+# explanation
+explainer = ExplainerWArgs(
+    explainer=IntegratedGradients(model)
+)
+attrs = explainer.attribute(inputs, targets, n_classes=1000)
 
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=4, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=4, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-        self.relu1 = nn.ReLU()
-        self.relu2 = nn.ReLU()
-        self.relu3 = nn.ReLU()
-        self.relu4 = nn.ReLU()
-
-    def forward(self, x):
-        x = self.pool1(self.relu1(self.conv1(x)))
-        x = self.pool2(self.relu2(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = self.relu3(self.fc1(x))
-        x = self.relu4(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-model = Net()
-model.load_state_dict(torch.load('models/cifar_torchvision.pt'))
-
-dataiter = iter(testloader)
-images, labels = next(dataiter)
-
-outputs = model(images)
-
-_, predicted = torch.max(outputs, 1)
-
-idx = 3
-
-inputs = images[idx].unsqueeze(0)
-inputs.requires_grad = True
-
-model.eval()
-
-
-project = Project('test_project')
-exp = project.explain([IntegratedGradients(model)])
-
-run = exp.run(inputs, target=labels[idx], baselines=inputs * 0, return_convergence_delta=True)
-plots = exp.visualize()
-
-plot_path = 'results/ig'
-if not os.path.exists(plot_path):
-    os.makedirs(plot_path)
-    
-for run_idx, run_plots in enumerate(plots):
-    for idx, plot in enumerate(run_plots):
-        plot.write_image(f"{plot_path}/ig_{run_idx}_{idx}.png")
+# evaluation
+metrics = [MuFidelity(n_perturbations=10), Sensitivity(n_iter=1), Complexity(n_bins=10)]
+evaluator = XaiEvaluator(metrics=metrics)
+evals = evaluator(
+    inputs=inputs,
+    targets=targets,
+    explainer_w_args=explainer,
+    explanations=attrs
+)
