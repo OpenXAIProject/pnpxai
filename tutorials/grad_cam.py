cd639a0d616c1f6ae64ac8440b4eca06f310bc96
gkim <gkim9213@gmail.com>
2023-12-20 15:17:39 +0900

    feat: modify tutorials for gradcams

--- tutorials/grad_cam.py
+++ tutorials/grad_cam.py
@@ -1,7 +1,6 @@
-from time import time_ns
-
+import plotly.express as px
 from torch.utils.data import DataLoader
-from pnpxai.explainers import GuidedGradCam, ExplainerWArgs
+from pnpxai.explainers import GradCam, ExplainerWArgs
 from pnpxai.evaluator import XaiEvaluator
 from pnpxai.evaluator.mu_fidelity import MuFidelity
 from pnpxai.evaluator.sensitivity import Sensitivity
@@ -9,15 +8,14 @@ from pnpxai.evaluator.complexity import Complexity
 
 from helpers import get_imagenet_dataset, get_torchvision_model
 
-model, transform = get_torchvision_model("inception_v3")
+model, transform = get_torchvision_model("resnet18")
 dataset = get_imagenet_dataset(transform=transform, subset_size=8)
 loader = DataLoader(dataset, batch_size=1)
 inputs, labels = next(iter(loader))
 explainer = ExplainerWArgs(
-    explainer = GuidedGradCam(model)
+    explainer = GradCam(model)
 )
 attrs = explainer.attribute(inputs, labels)
-
 evaluator = XaiEvaluator(metrics=[
     MuFidelity(n_perturbations=10, batch_size=4),
     Sensitivity(n_iter=1),
