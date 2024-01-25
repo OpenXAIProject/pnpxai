f9ee40c86967a498dfefc07c448ff6a6ccc76e99
Enver Menadjiev <enver1323@gmail.com>
2024-01-25 15:33:57 +0900

    feat: add api tests

--- pnpxai/tests/visualizer/backend/test_visualizer_app.py
+++ pnpxai/tests/visualizer/backend/test_visualizer_app.py
@@ -1,6 +1,6 @@
 import pytest
-from torch import nn
-from torch.utils.data import DataLoader
+from torch import nn, Tensor
+from torch.utils.data import DataLoader, Dataset
 
 from pnpxai import Project
 from pnpxai.visualizer.backend.app import create_app
@@ -8,11 +8,25 @@ from pnpxai.visualizer.server import Server
 from pnpxai.utils import class_to_string
 
 
+class DummyDataset(Dataset):
+    def __init__(self, data):
+        super(DummyDataset, self).__init__()
+        self._data = data
+
+    def __getitem__(self, idx: int):
+        return self._data[idx]
+
+    def __len__(self):
+        return len(self._data)
+
+
 class TestVisualizerApp:
     @pytest.fixture
     def app_projects(self):
         model = nn.Linear(1, 1)
-        loader = DataLoader([])
+        loader = DataLoader(DummyDataset([
+            (Tensor([0, 0, 0]), Tensor([0]))
+        ]))
 
         project1 = Project('project1')
         project1.create_auto_experiment(
@@ -44,5 +58,43 @@ class TestVisualizerApp:
             for exp_idx, (exp_name, exp) in enumerate(project.experiments.items()):
                 data_experiment = data_project['experiments'][exp_idx]
                 assert data_experiment['name'] == exp_name
-                explainers = [class_to_string(explainer) for explainer in exp.all_explainers]
+                explainers = [class_to_string(explainer)
+                              for explainer in exp.all_explainers]
                 assert data_experiment['explainers'] == explainers
+
+    def test_api_get_experiments(self, client, app_projects):
+        project = list(app_projects.values())[0]
+        exp_name, experiment = list(project.experiments.items())[0]
+        response = client.get(
+            f'/api/projects/{project.name}/experiments/{exp_name}/')
+        print(response)
+        payload = response.get_json()
+        assert payload['message'] == 'Success'
+
+    def test_api_get_experiments(self, client, app_projects):
+        project = list(app_projects.values())[0]
+        exp_name, experiment = list(project.experiments.items())[0]
+        response = client.get(
+            f'/api/projects/{project.name}/experiments/{exp_name}/')
+        print(response)
+        payload = response.get_json()
+        assert payload['message'] == 'Success'
+
+    def test_api_get_experiment_inputs(self, client, app_projects):
+        project = list(app_projects.values())[0]
+        exp_name, experiment = list(project.experiments.items())[0]
+        response = client.get(
+            f'/api/projects/{project.name}/experiments/{exp_name}/inputs/')
+        payload = response.get_json()
+        assert payload['message'] == 'Success'
+
+    def test_api_get_project_models(self, client, app_projects):
+        project = list(app_projects.values())[0]
+        exp_id = 0
+        exp_name, experiment = list(project.experiments.items())[exp_id]
+        response = client.get(
+            f'/api/projects/{project.name}/models/')
+        payload = response.get_json()
+        assert payload['message'] == 'Success'
+        assert payload['data'][exp_id]['name'] == class_to_string(
+            experiment.model)
