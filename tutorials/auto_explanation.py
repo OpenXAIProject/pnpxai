82f71c46ff1e4f02dc58f5add784800a939f6113
seongun-kim <seongun.1023@gmail.com>
2024-02-22 02:51:16 +0900

    doc: update README.md

--- tutorials/auto_explanation.py
+++ tutorials/auto_explanation.py
@@ -8,33 +8,46 @@ from pnpxai import Project
 from helpers import get_imagenet_dataset, get_torchvision_model, denormalize_image
 
 
-# -----------------------------------------------------------------------------#
-# ----------------------------------- setup -----------------------------------#
-# -----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
+#----------------------------------- setup -----------------------------------#
+#-----------------------------------------------------------------------------#
 
+# Set the seed for reproducibility
 set_seed(seed=0)
+
+# Determine the device based on the availability
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-def input_visualizer(x): return denormalize_image(x, transform.mean, transform.std)
-def target_visualizer(x): return dataset.dataset.idx_to_label(x.item())
 
-# -----------------------------------------------------------------------------#
-# ----------------------------------- Project -----------------------------------#
-# -----------------------------------------------------------------------------#
 
+#-----------------------------------------------------------------------------#
+#--------------------------------- project 1 ---------------------------------#
+#-----------------------------------------------------------------------------#
+
+# Initialize Project 1
 project = Project('Test Project 1')
 
-# -----------------------------------------------------------------------------#
-# ------------------------------------ Model1 -----------------------------------#
-# -----------------------------------------------------------------------------#
 
+#-----------------------------------------------------------------------------#
+#------------------------------- experiment 1-1 ------------------------------#
+#-----------------------------------------------------------------------------#
+
+# Load the model and its pre-processing transform for experiment 1-1
 model, transform = get_torchvision_model("resnet18")
 model = model.to(device)
+
+# Prepare the dataset and dataloader for experiment 1-1
 dataset = get_imagenet_dataset(transform, subset_size=25)
 loader = DataLoader(dataset, batch_size=10)
+
+# Define functions to extract input and target from the data loader
 def input_extractor(x): return x[0].to(device)
 def target_extractor(x): return x[1].to(device)
 
+# Define helper functions to visualize input and target
+def input_visualizer(x): return denormalize_image(x, transform.mean, transform.std)
+def target_visualizer(x): return dataset.dataset.idx_to_label(x.item())
 
+# Create an experiment to explain the defined model and dataset
 experiment_resnet = project.create_auto_experiment(
     model,
     loader,
@@ -45,18 +58,28 @@ experiment_resnet = project.create_auto_experiment(
     target_visualizer=target_visualizer,
 )
 
-# -----------------------------------------------------------------------------#
-# ---------------------------------- Model2 ----------------------------------#
-# -----------------------------------------------------------------------------#
 
+#-----------------------------------------------------------------------------#
+#------------------------------- experiment 1-2 ------------------------------#
+#-----------------------------------------------------------------------------#
+
+# Load the model and its pre-processing transform for experiment 1-2
 model, transform = get_torchvision_model("vit_b_16")
 model = model.to(device)
+
+# Prepare the dataset and dataloader for experiment 1-2
 dataset = get_imagenet_dataset(transform, subset_size=25)
 loader = DataLoader(dataset, batch_size=10)
+
+# Define functions to extract input and target from the data loader
 def input_extractor(x): return x[0].to(device)
 def target_extractor(x): return x[1].to(device)
 
+# Define helper functions to visualize input and target
+def input_visualizer(x): return denormalize_image(x, transform.mean, transform.std)
+def target_visualizer(x): return dataset.dataset.idx_to_label(x.item())
 
+# Create an experiment to explain the defined model and dataset
 experiment_vit = project.create_auto_experiment(
     model,
     loader,
@@ -68,11 +91,35 @@ experiment_vit = project.create_auto_experiment(
 )
 
 
-# Add Project 2 For Testing
+#-----------------------------------------------------------------------------#
+#--------------------------------- project 2 ---------------------------------#
+#-----------------------------------------------------------------------------#
+
+# Initialize Project 2
 project2 = Project('Test Project 2')
+
+
+#-----------------------------------------------------------------------------#
+#------------------------------- experiment 2-1 ------------------------------#
+#-----------------------------------------------------------------------------#
+
+# Load the model and its pre-processing transform for experiment 2-1
 model, transform = get_torchvision_model("vit_b_16")
 model = model.to(device)
 
+# Prepare the dataset and dataloader for experiment 2-1
+dataset = get_imagenet_dataset(transform, subset_size=25)
+loader = DataLoader(dataset, batch_size=10)
+
+# Define functions to extract input and target from the data loader
+def input_extractor(x): return x[0].to(device)
+def target_extractor(x): return x[1].to(device)
+
+# Define helper functions to visualize input and target
+def input_visualizer(x): return denormalize_image(x, transform.mean, transform.std)
+def target_visualizer(x): return dataset.dataset.idx_to_label(x.item())
+
+# Create an experiment to explain the defined model and dataset
 experiment_project2 = project2.create_auto_experiment(
     model,
     loader,
@@ -84,5 +131,9 @@ experiment_project2 = project2.create_auto_experiment(
 )
 
 
-# Running all project by running one of the projects#
+#-----------------------------------------------------------------------------#
+#----------------------------------- launch ----------------------------------#
+#-----------------------------------------------------------------------------#
+
+# Launch the interactive web-based dashboard by running one of the projects defined above
 project.get_server().serve(debug=True, host='0.0.0.0', port=5001)
